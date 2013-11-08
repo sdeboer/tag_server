4cf28931a760668dbb1ab1c6a436aa7894dcc198
Simon de Boer <sdeboer@ingamer.com>
2013-11-07 22:05:44 -0500

    Added the sets interface to persist.

--- src/persist.erl
+++ src/persist.erl
@@ -25,11 +25,18 @@
 	delist/1,
 	push/2, pop/1, pop/2,
 	unshift/2, shift/1, shift/2,
-	% remove/2,
-	members/1 %, ismember/2, size/1,
+	items/1 %, hasitem/2, length/1,
 	%range/3,
-	%intersection/1, interstore/2,
-	%union/1, unionstore/2
+	]).
+
+% Set API
+-export([
+	set/1, deset/1,
+	add/2, remove/2,
+	contains/2, members/1,
+	cardinality/1, card/1,
+	intersection/1, interstore/2,
+	union/1, unionstore/2
 	]).
 
 start_link() ->
@@ -41,10 +48,10 @@ mod_name()->
 
 % Keys
 del(K) ->
-	gen_server:call(mod_name(), {command, ["DEL", K]}).
+	command(["DEL", K]).
 
 exists(K)->
-	gen_server:call(mod_name(), {command, ["EXISTS", K]}).
+	command(["EXISTS", K]).
 
 % Models
 save(Key, Value) ->
@@ -71,18 +78,14 @@ delist(K) ->
 push([], S) -> S;
 
 push(List, S) ->
-	AddRes = command(["RPUSH", S#list.key | List]),
-	S2 = S#list{ persisted = true },
-	lager:debug("Add Result ~p", [AddRes]),
-	S2.
+	command(["RPUSH", S#list.key | List]),
+	S#list{ persisted = true }.
 
 unshift([], L) -> L;
 
 unshift(List, S) ->
-	AddRes = command(["LPUSH", S#list.key | List]),
-	S2 = S#list{ persisted = true },
-	lager:debug("Add Result ~p", [AddRes]),
-	S2.
+	command(["LPUSH", S#list.key | List]),
+	S#list{ persisted = true }.
 
 pop(L) ->
 	% check undefined (empty list)
@@ -112,10 +115,68 @@ shift(N, L)->
 	end,
 	{Vs, L}.
 
-members(S) ->
+items(S) ->
 	V = command(["LRANGE", S#list.key, 0, -1]),
 	lager:debug("MEMBERS ~p", [V]),
 	{V, S#list{persisted = true, dirty = false} }.
+
+% Sets
+
+-record(set, { key, persisted = false, dirty = true }).
+
+set(K) -> #set{key = K}.
+
+deset(S) when is_record(S, set) ->
+	del(S#set.key),
+	{ok, S#set{persisted = false, dirty = true}};
+
+deset(K) ->
+	del(K),
+	{ok, #set{key = K}}.
+
+add(List, S) ->
+	command(["SADD", S#set.key | List]),
+	S#set{ persisted = true }.
+
+remove(List, S) ->
+	command(["SREM", S#set.key | List]).
+
+contains(Item, S) ->
+	case command(["SISMEMBER", S#set.key, Item]) of
+		<<"1">> -> true;
+		<<"0">> -> false
+	end.
+
+members(S) ->
+	command(["SMEMBERS", S#set.key]).
+
+cardinality(S) ->
+	command(["SCARD", S#set.key]).
+
+card(S) -> cardinality(S).
+
+keylist(SList) ->
+	lists:map(fun(S) -> S#set.key end, SList).
+
+intersection(SList) ->
+	command(["SINTER" | keylist(SList)]).
+
+interstore(Dest, SList) when is_record(Dest, set) ->
+	command(["SINTERSTORE", Dest#set.key, keylist(SList)]),
+	Dest#set{persisted = true};
+
+interstore(DK, SList) ->
+	interstore(#set{key = DK}, SList).
+
+union(SList) ->
+	command(["SUNION" | keylist(SList)]).
+
+unionstore(Dest, SList) when is_record(Dest, set) ->
+	command(["SUNIONSTORE", Dest#set.key | keylist(SList)]),
+	Dest#set{persisted = true};
+
+unionstore(DK, SList) ->
+	unionstore(#set{key = DK}, SList).
 
 init([]) ->
 	{ok, Redis} = eredis:start_link(),
